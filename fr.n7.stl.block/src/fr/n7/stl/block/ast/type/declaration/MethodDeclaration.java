d62926f3a88ebc24b654ab434fa26dfdfb2e2f20
ajeanvoi <jeanvoine.achille@gmail.com>
2024-05-17 11:44:31 +0200

    debut class type

---
+++ fr.n7.stl.block/src/fr/n7/stl/block/ast/type/declaration/MethodDeclaration.java
@@ -0,0 +1,20 @@
+package fr.n7.stl.block.ast.type.declaration;
+
+import fr.n7.stl.block.ast.scope.Declaration;
+import fr.n7.stl.block.ast.type.Type;
+
+public class MethodDeclaration implements Declaration{
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
