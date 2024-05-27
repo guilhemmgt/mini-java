5624497789d7f18b3ecd31f5a251b08dd2950cf9
Claire0607 <clairescheyder1@gmail.com>
2024-05-27 21:35:14 +0200

    debut methodes et constructeurs

---
+++ fr.n7.stl.block/src/fr/n7/stl/block/ast/expression/ConstructorCall.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package fr.n7.stl.block.ast.expression;
+
+import java.util.Iterator;
+import java.util.List;
+
+import fr.n7.stl.block.ast.SemanticsUndefinedException;
+import fr.n7.stl.block.ast.instruction.declaration.ConstructorDeclaration;
+import fr.n7.stl.block.ast.instruction.declaration.FunctionDeclaration;
+import fr.n7.stl.block.ast.instruction.declaration.MethodDeclaration;
+import fr.n7.stl.block.ast.scope.Declaration;
+import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.type.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.Register;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+public class ConstructorCall implements Expression {
+
+	protected ConstructorDeclaration methode;
+	protected List<Expression> arguments;
+	
+	/**
+	 * @param _methode : called function.
+	 * @param _arguments : List of AST nodes that computes the values of the parameters for the function call.
+	 */
+	public ConstructorCall(ConstructorDeclaration _methode, List<Expression> _arguments) {
+		this.methode = _methode;
+		this.arguments = _arguments;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		String _result = ((this.methode == null)?this.methode.getName():this.methode) + "( ";
+		Iterator<Expression> _iter = this.arguments.iterator();
+		if (_iter.hasNext()) {
+			_result += _iter.next();
+		}
+		while (_iter.hasNext()) {
+			_result += " ," + _iter.next();
+		}
+		return  _result + ")";
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.expression.Expression#collect(fr.n7.stl.block.ast.scope.HierarchicalScope)
+	 */
+	@Override
+	public boolean collectAndBackwardResolve(HierarchicalScope<Declaration> _scope) {
+		boolean funCollect = this.methode == null ? true : this.methode.collectCE(_scope);
+		boolean argCollects = true;
+		for (Expression arg : this.arguments) {
+			argCollects = argCollects && arg.collectAndBackwardResolve(_scope);
+		}
+		return funCollect && argCollects;
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.expression.Expression#resolve(fr.n7.stl.block.ast.scope.HierarchicalScope)
+	 */
+	@Override
+	public boolean fullResolve(HierarchicalScope<Declaration> _scope) {
+		boolean funResolve = this.methode.resolveCE(_scope);
+		boolean argResolves = true;
+		for (Expression arg : this.arguments) {
+			argResolves = argResolves && arg.fullResolve(_scope);
+		}
+		return funResolve && argResolves;
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Expression#getType()
+	 */
+	@Override
+	public Type getType() {
+		//throw new SemanticsUndefinedException( "Semantics getType is undefined in FunctionCall.");
+		return this.methode.getType();
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Expression#getCode(fr.n7.stl.tam.ast.TAMFactory)
+	 */
+	@Override
+	public Fragment getCode(TAMFactory _factory) {
+		//throw new SemanticsUndefinedException( "Semantics getCode is undefined in FunctionCall.");
+		Fragment frag = _factory.createFragment();
+		frag.addComment(this.toString());
+		for (Expression e : this.arguments) {
+			frag.append(e.getCode(_factory));
+		}
+		frag.add(_factory.createCall(this.methode.getName(), Register.SB));
+		return frag;
+	}
+}
--- fr.n7.stl.block/src/fr/n7/stl/block/ast/instruction/declaration/ConstructorDeclaration.java
+++ fr.n7.stl.block/src/fr/n7/stl/block/ast/instruction/declaration/ConstructorDeclaration.java
@@ -2,9 +2,11 @@ package fr.n7.stl.block.ast.instruction.declaration;
 
 import java.util.List;
 
+import fr.n7.stl.block.ast.Block;
 import fr.n7.stl.block.ast.instruction.ClassElement;
 import fr.n7.stl.block.ast.scope.Declaration;
 import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.scope.SymbolTable;
 import fr.n7.stl.block.ast.type.AccessRight;
 import fr.n7.stl.block.ast.type.Type;
 
@@ -12,16 +14,18 @@ public class ConstructorDeclaration implements ClassElement {
 	private AccessRight typeAcces = null;
 	private String name;
 	private List<ParameterDeclaration> parameters;
+	private Block corps;
 
-	public ConstructorDeclaration(String name, List<ParameterDeclaration> parameters) {
+	HierarchicalScope<Declaration> scopeParams;
+
+	public ConstructorDeclaration(String name, List<ParameterDeclaration> parameters, Block corps) {
 		this.name = name;
 		this.parameters = parameters;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
 
 	@Override
@@ -49,6 +53,18 @@ public class ConstructorDeclaration implements ClassElement {
 	@Override
 	public boolean collectCE(HierarchicalScope<Declaration> _scope) {
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'collectCE'");
+		this.scopeParams = new SymbolTable(_scope);
+		
+		if (_scope.accepts(this)) {
+			_scope.register(this);
+			
+			for(ParameterDeclaration pad : parameters) {
+				this.scopeParams.register(pad);
+			}
+			
+			return this.corps.collect(this.scopeParams);
+		} else {
+			return false;
+		}
 	}
 }
